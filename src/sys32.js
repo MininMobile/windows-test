4757d52835c47e2a2f8df5d299741a324b052a31
MininMobile <miningamer@gmail.com>
2018-05-26 17:26:12 +0100

    Add MoveToFront() - Comment

--- src/sys32.js
+++ src/sys32.js
@@ -11,7 +11,7 @@ var appdata = {
 			author:"Microsoft",
 			description:"Browser your files"
 		},
-		init:(w) => { w.window.classList.add("explorer") },
+		init:(w) => { console.log("Explorer Initialized!") },
 		content:`<center><h1>Files, files, files...</h1><center>`
 	}
 }
@@ -22,27 +22,47 @@ class Window {
 	constructor(appid = "0") {
 		const body = document.body;
 
+		// Get App Data
 		this.appid = appid;
 		this.appdata = appdata[apps[this.appid]];
 
+		// Create Base Element
 		this.window = document.createElement("div");
 		this.window.classList.add("window");
-		
+
+		// Title Bar
 		this.titlebar = document.createElement("div");
 		this.titlebar.classList.add("window-title");
 		this.titlebar.onmousedown = (e) => windowDragEvent(e, this.window);
 
+		// Window Content
 		this.content = document.createElement("div");
 		this.content.classList.add("window-content");
 		this.content.innerHTML = this.appdata.content;
 		
-		if (apps[appid]) this.window.classList.add(apps[appid]);
+		// if app exists, add appid to classlist
+		if (apps[appid]) this.window.classList.add(`app-${this.appid}`);
 
+		// Append Children to Base Window Element
 		this.window.appendChild(this.titlebar);
 		this.window.appendChild(this.content);
 		body.appendChild(this.window);
 
+		// Attach Event Listeners
+		this.window.onclick = (e) => {
+			MoveToFront(e);
+		}
+
+		// Initialize App
 		this.appdata.init(this);
+	}
+
+	/**
+	 * Move Window to Front on Click
+	 * @param {MouseEvent} e
+	 */
+	MoveToFront(e) {
+
 	}
 }
 
