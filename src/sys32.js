4f19376e8b2106e2a09efd858ca8e10c9a1ad68e
MininMobile <miningamer@gmail.com>
2018-05-26 14:50:23 +0100

    Add Window types - Add Window Class - Add addWindow Function

---
+++ src/sys32.js
@@ -0,0 +1,15 @@
+var apps = {
+	"0":"explorer"
+}
+
+class Window {
+	constructor(appid = "0") {
+		const body = document.body;
+
+		body.appendChild();
+	}
+}
+
+function addWindow() {
+	return new Window();
+}
